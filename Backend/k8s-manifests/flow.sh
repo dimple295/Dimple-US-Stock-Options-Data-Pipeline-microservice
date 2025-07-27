cfe3eb170689e3e7c657cab76166cb9b32b7b6d2
HARSHDEEP MAHAJAN <65184650+hdm08@users.noreply.github.com>
2025-07-27 15:04:53 +0100

    Azure load balancer

--- Backend/k8s-manifests/flow.sh
+++ Backend/k8s-manifests/flow.sh
@@ -1,18 +1,19 @@
 # delete old cluster
-kind delete cluster --name stock-cluster
+# kind delete cluster --name stock-cluster
 
-# creating Cluster
-echo "Creating Cluster: stock-cluster"
-kind create cluster --config cluster/kind-config.yaml 
-echo "stock-cluster Created"
+# # creating Cluster
+# echo "Creating Cluster: stock-cluster"
+# kind create cluster --config cluster/kind-config.yaml 
+# echo "stock-cluster Created"
 
 # update helm package and load images
 cd script
 chmod +x helm.sh
-chmod +x load_image.sh
+cd scrip
+chmod +x build_image.sh
 ./helm.sh
 ./load_image.sh
-cd ..
+cd ../
 
 # apply manifest
 kubectl apply -f manifests
