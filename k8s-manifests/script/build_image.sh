bffc7d7d5f209bca6c5966674f3c146604b9f113
HARSHDEEP MAHAJAN <65184650+hdm08@users.noreply.github.com>
2025-08-09 23:47:58 +0100

    Fixed the repo structure

--- k8s-manifests/script/build_image.sh
+++ k8s-manifests/script/build_image.sh
@@ -8,7 +8,7 @@ services=(
   "data_collector_service"
   "data_processor_service"
   "database_writer_service"
-  "file_writer_service"
+  # "file_writer_service"
   "data_api_service"
 )
 # #_____________________Local image___________________________________________________
@@ -17,17 +17,24 @@ for service in "${services[@]}"; do
   echo "📦 Building and pushing: $service"
   docker buildx build \
     --platform linux/arm64 \
-    -t ${DOCKER_HUB_USERNAME}/${service}_arm2 \
+    -t ${DOCKER_HUB_USERNAME}/${service}_arm10 \
     --load \
     "${BASE_PATH}Backend/${service}"
 done
-# docker buildx build \
-#     --platform linux/arm64 \
-#     -t ${DOCKER_HUB_USERNAME}/frontend_arm2 \
-#     --push \
-#     "${BASE_PATH}Frontend/Stock_Analysis_Frontend"
 
+# #-----File writer with amd architecture
+docker buildx build \
+    --platform linux/amd64 \
+    -t hdm08/file_writer_service_arm10 \
+    --load  \
+    "${BASE_PATH}Backend/file_writer_service"
 
+# #-----Frontend
+docker buildx build \
+    --platform linux/arm64 \
+    -t ${DOCKER_HUB_USERNAME}/frontend_arm10 \
+    --load \
+    "${BASE_PATH}Frontend/Stock_Analysis_Frontend"
 
 # #________________________docker hub________________________________________________
 
@@ -35,13 +42,21 @@ for service in "${services[@]}"; do
   echo "📦 Building and pushing: $service"
   docker buildx build \
     --platform linux/arm64 \
-    -t ${DOCKER_HUB_USERNAME}/${service}_arm2 \
+    -t ${DOCKER_HUB_USERNAME}/${service}_arm10 \
     --push \
     "${BASE_PATH}Backend/${service}"
 done
 
-# docker buildx build \
-#     --platform linux/arm64 \
-#     -t ${DOCKER_HUB_USERNAME}/frontend_arm2 \
-#     --push \
-#     "${BASE_PATH}Frontend/Stock_Analysis_Frontend"
+# #-----File writer with amd architecture
+docker buildx build \
+    --platform linux/amd64 \
+    -t hdm08/file_writer_service_arm10 \
+    --push  \
+    "${BASE_PATH}Backend/file_writer_service"
+
+# #-----Frontend
+docker buildx build \
+    --platform linux/arm64 \
+    -t ${DOCKER_HUB_USERNAME}/frontend_arm10 \
+    --push \
+    "${BASE_PATH}Frontend/Stock_Analysis_Frontend"
